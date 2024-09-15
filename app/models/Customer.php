bd6be2eaa834bfe95149ecfb418c93d53a662b66
CodeOfTerminator <114341536+CodeOfTerminator@users.noreply.github.com>
2024-09-15 22:18:52 +0800

    erm
    my user part

--- app/models/Customer.php
+++ app/models/Customer.php
@@ -1,7 +1,14 @@
 <?php
 
+require_once './User.php';
+
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 
+class Customer extends User {
+    protected $table = 'Customer';
+    
+    public 
+}
